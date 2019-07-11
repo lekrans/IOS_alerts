b92de7e87e18e967b1e009e6872f7a6c21d473c9
Michael Lekrans <mlekrans@hotmail.com>
2019-07-11 08:36:09 +0200

    Added button and action sheet method

--- IOS_alerts/ViewController.swift
+++ IOS_alerts/ViewController.swift
@@ -15,6 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert(_ sender: Any) {
+        // create alert
+        let alert: UIAlertController = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
+        // create action
+        let action1:UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) { (_:UIAlertAction) in
+            print("cancel handler")
+        }
+        // add action
+        alert.addAction(action1)
+        // show alert
+        self.present(alert, animated: true) {
+            print("Alert handler")
+        }
+    }
+    
+    @IBAction func showAlertAsActionSheet(_ sender: Any) {
+        let alert: UIAlertController = UIAlertController(title: "Title actionsheet", message: "Message actionsheet", preferredStyle: .actionSheet) // <- .actionSheet
+        // create action
+        let action1:UIAlertAction = UIAlertAction(title: "delete", style: .destructive) { (_:UIAlertAction) in
+            print("delete handler actionsheet")
+        }
+        // add action
+        alert.addAction(action1)
+        // show alert
+        self.present(alert, animated: true) {
+            print("Alert handler ActionSheet")
+        }
+    }
 }
 
